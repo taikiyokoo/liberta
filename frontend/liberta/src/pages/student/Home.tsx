15500d82701ce6bbfd2d133d29dd14f2d4609873
taikiyokoo <yokootaiki1126@gmail.com>
2023-04-09 22:33:21 +0900

    ログイン後ユーザーを識別してページ遷移、全ユーザーを取得して生徒先生別で表示

--- frontend/liberta/src/pages/student/Home.tsx
+++ frontend/liberta/src/pages/student/Home.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { Grid } from '@mui/material'
+import { User } from 'interfaces'
+import { getUsers } from 'pages/api/user'
+import StudentCard from 'pages/components/Cards/StudentCard'
+import TeacherCard from 'pages/components/Cards/TeacherCard'
+import UserCard from 'pages/components/UserCard'
+import { AuthContext } from 'pages/_app'
+import React, { useContext, useEffect, useState } from 'react'
 
 const Home = () => {
+
+  const [teachers,setTeachers] = useState<User[]>([])
+  const {loading,setLoading} =useContext(AuthContext)
+
+  const handleGetUsers = async () => {
+
+    try{
+      const res = await getUsers()
+      const users:User[] = res.data
+      users.forEach((user: User)=>{
+        if(user.teacherProfile){
+          setTeachers([...teachers, user])
+        }
+      })
+
+    }catch(err){
+      console.log(err)
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {handleGetUsers()}, [])
   return (
     <div>
-      
+          <Grid container sx={{width: "100%"}} rowSpacing={6} columnSpacing={{ xs: 1, sm: 2, md: 3 }} justifyContent="center">
+            {teachers.map((user: User)=>{
+              return(
+                <Grid item key={user.id} >
+                  <StudentCard  user={user}/>
+                </Grid>
+              )
+            })}
+          </Grid>
     </div>
   )
 }
